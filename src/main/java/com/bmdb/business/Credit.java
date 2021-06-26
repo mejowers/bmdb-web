1b31c900751945861efba0db993bfc7b30088798
Michelle Jowers <michelle.jowers@icloud.com>
2021-06-26 14:03:56 -0400

    adding files

---
+++ src/main/java/com/bmdb/business/Credit.java
@@ -0,0 +1,95 @@
+package com.bmdb.business;
+
+public class Credit {
+	//instance variables
+	private int id;
+	private Actor actor;
+	private Movie movie;
+	private String role;
+	
+	public Credit(int id, Actor actor, Movie movie, String role) {
+		super();
+		this.id = id;
+		this.actor = actor;
+		this.movie = movie;
+		this.role = role;
+	}
+	
+	public Credit(Actor actor, Movie movie, String role) {
+		super();
+		this.actor = actor;
+		this.movie = movie;
+		this.role = role;
+	}
+	
+	public Credit() {
+		super();
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the actor
+	 */
+	public Actor getActor() {
+		return actor;
+	}
+
+	/**
+	 * @param actor the actor to set
+	 */
+	public void setActor(Actor actor) {
+		this.actor = actor;
+	}
+
+	/**
+	 * @return the movie
+	 */
+	public Movie getMovie() {
+		return movie;
+	}
+
+	/**
+	 * @param movie the movie to set
+	 */
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+	/**
+	 * @return the role
+	 */
+	public String getRole() {
+		return role;
+	}
+
+	/**
+	 * @param role the role to set
+	 */
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "Credit [id=" + id + ", actor=" + actor + ", movie=" + movie + ", role=" + role + "]";
+	}
+	
+public String getCreditString() {
+	return "Credit ID: "+id+"\n"+
+			"Movie = " +movie.getTitle()+ ", ("+movie.getYear()+"), Rating: " +movie.getRating() +"\n"+
+			"Actor = "+actor.getFullName()+" Role: "+role;
+}
+}
---
+++ src/main/java/com/bmdb/business/Movie.java
@@ -0,0 +1,84 @@
+package com.bmdb.business;
+
+import javax.persistence.*;
+
+@Entity
+public class Movie {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String title;
+	private int year;
+	private String rating;
+	private String director;
+
+	public Movie(int id, String title, String rating, int year, String director) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.rating = rating;
+		this.year = year;
+		this.director = director;
+	}
+	public Movie(String title, String rating, int year, String director) {
+		super();
+		this.title = title;
+		this.rating = rating;
+		this.year = year;
+		this.director = director;
+	}
+	
+	public Movie() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int newYear) {
+		this.year = newYear;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	@Override
+	public String toString() {
+		return 	"\n"+
+				"Movie id: "+id+"\n"+
+				"Title: "+title+", Rating: "+rating+"\n"+
+				"Year: "+year+", Director: "+director;
+				
+	}
+
+}
